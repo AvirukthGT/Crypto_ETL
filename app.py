47d62b41de4768a716987653b2c07a5accfb931d
Randxm4est <avirukth@gmail.com>
2025-05-05 18:22:06 +1000

    5th may 2025 data

--- app.py
+++ app.py
@@ -10,4 +10,27 @@ param = {
     'page': 1
 }
 
+response=requests.get(url,params=param)
+
+if response.status_code==200:
+    print("Connection Successful")
+    data=response.json()
+
+    df=pd.DataFrame(data)
+
+    # print(df.columns)
+    # print(df.head())
+
+    df=df[['id','current_price', 'market_cap','price_change_percentage_24h','ath','atl']]
+
+    today=datetime.now().strftime('%d-%m-%Y')
+    df['time_stamp']=today
+
+    df.to_csv("crypto_{}.csv".format(today),index=False)
+    print("Data Saved Successfully!!!")
+else:
+    print(f"Connection Failed,Error Code:{response.status_code}")
+
+
+
 
