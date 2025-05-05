750ab9fdfa31398b546a0013edce47733aad9b77
Randxm4est <avirukth@gmail.com>
2025-05-05 18:33:12 +1000

    Top 10 positive and negative movers

--- app.py
+++ app.py
@@ -23,8 +23,13 @@ if response.status_code==200:
 
     df=df[['id','current_price', 'market_cap','price_change_percentage_24h','ath','atl']]
 
-    today=datetime.now().strftime('%d-%m-%Y')
+    today=datetime.now().strftime('%d-%m-%Y  %H-%M-%S')
     df['time_stamp']=today
+
+    top_negative=df.sort_values(by='price_change_percentage_24h',ascending=True).head(10)
+    top_negative.to_csv("top_negative_{}.csv".format(today),index=False)
+    top_positive=df.sort_values(by='price_change_percentage_24h',ascending=False).head(10)
+    top_positive.to_csv("top_positive_{}.csv".format(today),index=False)
 
     df.to_csv("crypto_{}.csv".format(today),index=False)
     print("Data Saved Successfully!!!")
